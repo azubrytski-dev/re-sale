c53cbb893987524194e613a8e8620c4d5ff570e9
andrei-z <azubrytski.dev@gmail.com>
2022-11-23 23:16:47 +0300

    init commit

---
+++ re-sale.app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello from the beauty store</h1>
+
+    </div>
+  )
+}
+
+export default App
